b4c5a85a3114769b8b3c300fed5c7ab3ccd774b3
menghuiding <dingmenghui026@163.com>
2025-03-29 15:23:43 +0800

    fix: del project

--- src/components/RightContent/index.tsx
+++ src/components/RightContent/index.tsx
@@ -1,32 +1,33 @@
-import { updateSelectProjectId } from '@/services/ant-design-pro/api';
-import { Select } from 'antd';
-import { useModel } from 'umi';
+// import { updateSelectProjectId } from '@/services/ant-design-pro/api';
+// import { Select } from 'antd';
+// import { useModel } from 'umi';
 
 export type SiderTheme = 'light' | 'dark';
 
 export const Question = () => {
-  const { initialState } = useModel('@@initialState');
-  const defaultProject = initialState?.currentUser?.selectProject;
-  const projectList = initialState?.projectList?.map((item: any) => ({
-    value: item.id,
-    label: item.name,
-  }));
+  // const { initialState } = useModel('@@initialState');
+  // const defaultProject = initialState?.currentUser?.selectProject;
+  // const projectList = initialState?.projectList?.map((item: any) => ({
+  //   value: item.id,
+  //   label: item.name,
+  // }));
 
-  const handleOnChange = (e: any) => {
-    updateSelectProjectId({ selectProject: e.value });
-    location.reload();
-  };
+  // const handleOnChange = (e: any) => {
+  //   updateSelectProjectId({ selectProject: e.value });
+  //   location.reload();
+  // };
 
   return (
     <div>
-      项目：
+      {/* 租户信息，暂时不需要 */}
+      {/* 项目：
       <Select
         defaultValue={Number(defaultProject)}
         style={{ width: 125 }}
         options={projectList}
         labelInValue
         onChange={(e) => handleOnChange(e)}
-      />
+      /> */}
     </div>
   );
 };
